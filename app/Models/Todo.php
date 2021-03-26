3bb8147ca3cfbe87fbbfbf98b79d31a19237f4df
Mumuni Mohammed <mumunim10@gmail.com>
2021-03-26 23:06:34 +0000

    Updated models.

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
